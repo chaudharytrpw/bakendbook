8e947243857e929d652402cdd194a52e6fdd769d
Mritunjay Chaudhary <mritunjaychaudhary73@gmail.com>
2026-03-10 17:47:03 +0530

    add file

--- config/db.js
+++ config/db.js
@@ -1,17 +1,13 @@
+// db.js
 const { Pool } = require("pg");
-require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }  
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Required for Supabase
 });
 
 pool.on("connect", () => {
-  console.log("PostgreSQL connected");
+  console.log("PostgreSQL connected (Supabase)");
 });
 
 module.exports = pool;
