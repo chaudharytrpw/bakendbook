34c6e3ad7e4ab67c9e02b4db4a74d75583a411e8
Mritunjay Chaudhary <mritunjaychaudhary73@gmail.com>
2026-03-10 18:19:07 +0530

    add file

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
-// db.js
 const { Pool } = require("pg");
 
+// Local run check
+const isLocal = process.env.NODE_ENV !== "production";
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Required for Supabase
+  connectionString: isLocal ? process.env.DATABASE_URL_LOCAL : process.env.DATABASE_URL,
+  ssl: isLocal
+    ? false // Local me SSL off
+    : { rejectUnauthorized: false }, // Vercel serverless me SSL required
 });
 
 pool.on("connect", () => {
-  console.log("PostgreSQL connected (Supabase)");
+  console.log("PostgreSQL connected");
 });
 
 module.exports = pool;
